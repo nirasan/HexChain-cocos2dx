0ab095206190e13e2eba7bd03daeccdc4b566947
nirasan <chibag+nirasan@gmail.com>
2013-09-23 13:43:30 +0900

    init Test

---
+++ proj.ios/HexChainTest/HexChainTest.h
@@ -0,0 +1,13 @@
+//
+//  HexChainTest.h
+//  HexChainTest
+//
+//  Created by 千葉 大二郎 on 2013/09/23.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HexChainTest : SenTestCase
+
+@end
---
+++ proj.ios/HexChainTest/HexChainTest.m
@@ -0,0 +1,32 @@
+//
+//  HexChainTest.m
+//  HexChainTest
+//
+//  Created by 千葉 大二郎 on 2013/09/23.
+//
+//
+
+#import "HexChainTest.h"
+
+@implementation HexChainTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //STFail(@"Unit tests are not implemented yet in HexChainTest");
+}
+
+@end
